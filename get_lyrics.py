a37455b6c87b8fd42018041ba01169d36cccb3ac
Nikolay S <deluxoft@gmail.com>
2017-11-21 23:31:41 +0300

    cleaned code

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
 import sys
 import os
 import json
-from os.path import join as opj
-from os.path import exists as ope
-from itertools import imap
-from itertools import ifilter
-from itertools import izip
 import requests
 from bs4 import BeautifulSoup as BS4
 
@@ -18,15 +12,25 @@ sys.setdefaultencoding('utf8')
 
 
 def by_search_str(item, search_str):
+    """checks if item fits good for search_str string
+
+    :param item: dict-like json object
+    :param search_str: string we're searching for
+    """
     lowered = map(str.strip, search_str.split(' '))
     full_title = str(item['result']['full_title']).lower().strip()
     return all(x in full_title for x in lowered)
 
 
 def get_lyrics(url, bs4_backend='lxml'):
+    """returns lyrics of song located at url
+
+    :param url: string of URL
+    :param bs4_backend: backend for beautifulSoup4
+    """
     response = requests.get(url)
     if not response.ok:
-        # TODO
+        print 'Something\'s wrong with %s' % url
         return str()
 
     # we use lxml instead of default html.parser
@@ -35,7 +39,7 @@ def get_lyrics(url, bs4_backend='lxml'):
     page_data = soup.select_one('meta[itemprop=page_data]')
 
     if not page_data:
-        # TODO
+        print 'Can\'t detect meta tag with itemprop=page_data property'
         return str()
 
     json_value = json.loads(page_data['content'])
@@ -47,38 +51,29 @@ def get_lyrics(url, bs4_backend='lxml'):
     return BS4(markup, bs4_backend).text.strip()
 
 
-if __name__ == '__main__':
-    # print sys.argv
-
+def main():
+    # read https://docs.genius.com/
     ACCESS_TOKEN = os.environ.get('GENIUS_ACCESS_TOKEN')
-
-    sample_artist = 'Madonna'
-    sample_song = 'girl gone wild'
 
     API_HOST = 'https://api.genius.com'
     HOST = 'https://genius.com'
     SEARCH_ENDPOINT = '%s/search' % API_HOST
-    SONGS_ENDPOINT = '%s/songs' % API_HOST
 
     # make sure we have everything needed making requests to genius.com
     required = (ACCESS_TOKEN, )
     if any(x is None for x in required):
         messages = list()
         if ACCESS_TOKEN is None:
-            messages.append('Something\'s wring with Client Access Token')
+            messages.append('Something\'s wrong with Client Access Token')
 
         print '\n'.join(messages)
         sys.exit(1)
 
     # special oauth2 header with `Bearer`, see documentation
-    headers = {
-        'Authorization': 'Bearer %s' % ACCESS_TOKEN
-    }
+    headers = {'Authorization': 'Bearer %s' % ACCESS_TOKEN}
 
     # params for get request
-    params = {
-        'q': 'madonna girl gone wild'
-    }
+    params = {'q': 'madonna girl gone wild'}
 
     response = requests.get(SEARCH_ENDPOINT, headers=headers, params=params)
     if response.ok:
@@ -94,21 +89,12 @@ if __name__ == '__main__':
             url = '{host}{path}'.format(host=HOST, path=path)
             lyrics = get_lyrics(url)
             if lyrics:
-
-            else:
                 # TODO
                 pass
         else:
-            # TODO
-            print 'NOT FOUND'
-            pass
+            print 'It semms like song was not found'
     else:
-        # TODO
-        pass
+        print 'Can\'t make a search for your song'
 
-
-    # sample_song = 'Never Gonna Give You Up'
-
-    # headers = {
-        # 'Authorization': 'B'
-    # }
+if __name__ == '__main__':
+    main()

1e7a83617f0d86392112dd73b781e440e0b99f1c
Nikolay S <deluxoft@gmail.com>
2017-11-21 21:27:07 +0300

    result of response added and some addition in the script

--- get_lyrics.py
+++ get_lyrics.py
@@ -18,9 +18,18 @@ sys.setdefaultencoding('utf8')
 
 
 if __name__ == '__main__':
-    ACCESS_TOKEN = os.environ.get('GENIUS_ACCESS_TOKEN')
-    ENDPOINT = 'https://api.genius.com/search'
+    print sys.ARGV
 
+    ACCESS_TOKEN = os.environ.get('GENIUS_ACCESS_TOKEN')
+
+    sample_artist = 'Madonna'
+    sample_song = 'girl gone wild'
+
+    API_HOST = 'https://api.genius.com'
+    SEARCH_ENDPOINT = '%s/search' % API_HOST
+    ARTISTS_ENDPOINT = '%s/artists' % API_HOST
+
+    # make sure we have everything needed making requests to genius.com
     required = (ACCESS_TOKEN, )
     if any(x is None for x in required):
         messages = list()
@@ -30,16 +39,28 @@ if __name__ == '__main__':
         print '\n'.join(messages)
         sys.exit(1)
 
+    # special oauth2 header with `Bearer`, see documentation
     headers = {
         'Authorization': 'Bearer %s' % ACCESS_TOKEN
     }
+
+    # params for get request
     params = {
-        'q': 'Never Gonna Give You Up'
+        'q': 'madonna girl gone wild'
     }
 
-    response = requests.get(ENDPOINT, headers=headers, params=params)
-    print response.status_code
+    response = requests.get(SEARCH_ENDPOINT, headers=headers, params=params)
+    if response.ok:
+        json_response = response.json()
 
+        # there're multiple search results sent be their server, so
+        # we have to filter them and to look for interesting one for us
+        search_results = json_response['response']['hits']
+        # TODO
+        # results_filtered = 
+    else:
+        # TODO
+        pass
 
 
     # sample_song = 'Never Gonna Give You Up'

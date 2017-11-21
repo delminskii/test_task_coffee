d682b11da1e2cbac50242393b4b44c0df8c547f6
Nikolay <deluxoft@gmail.com>
2017-11-21 19:56:11 +0300

    Add files via upload

---
+++ get_lyrics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import csv
+import sys
+import os
+import json
+from os.path import join as opj
+from os.path import exists as ope
+from itertools import imap
+from itertools import ifilter
+from itertools import izip
+import requests
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+if __name__ == '__main__':
+    pass

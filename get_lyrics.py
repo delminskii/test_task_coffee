fe652f50c241d0fa695f36117587dc556c1c89c3
Nikolay S <deluxoft@gmail.com>
2017-11-21 21:00:02 +0300

    start writing

--- get_lyrics.py
+++ get_lyrics.py
@@ -18,4 +18,32 @@ sys.setdefaultencoding('utf8')
 
 
 if __name__ == '__main__':
-    pass
+    ACCESS_TOKEN = os.environ.get('GENIUS_ACCESS_TOKEN')
+    ENDPOINT = 'https://api.genius.com/search'
+
+    required = (ACCESS_TOKEN, )
+    if any(x is None for x in required):
+        messages = list()
+        if ACCESS_TOKEN is None:
+            messages.append('Something\'s wring with Client Access Token')
+
+        print '\n'.join(messages)
+        sys.exit(1)
+
+    headers = {
+        'Authorization': 'Bearer %s' % ACCESS_TOKEN
+    }
+    params = {
+        'q': 'Never Gonna Give You Up'
+    }
+
+    response = requests.get(ENDPOINT, headers=headers, params=params)
+    print response.status_code
+
+
+
+    # sample_song = 'Never Gonna Give You Up'
+
+    # headers = {
+        # 'Authorization': 'B'
+    # }
